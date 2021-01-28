724d31e7a2925b3e782ee67326571b8c97b744c7
Yidnekachew Kibru <yidnekibru@gmail.com>
2021-01-28 09:43:06 +0300

    Add project files.

---
+++ msgBox/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace msgBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = "Notepad";
+            string msg = "Do you want to save changes ?";
+            DialogResult x = MessageBox.Show(msg, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (x == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
